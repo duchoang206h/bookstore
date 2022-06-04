01ed24a78fa0aaa8ecae8f326da39e202cc0907b
duchoang206h@gmail.com <duchoang206h@gmail.com>
2022-06-04 16:10:15 +0700

    chore: remove garbage files, directories

--- src/auth/router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { callback } = require('./callback')
-const passport = require('./passport')
-router.get('/login/google',passport.authenticate('google', { scope: ['email','profile','openid'] })) 
-router.get('/login/github',passport.authenticate('github', { scope: ['user:email'] }))
-
-router.get('/login/google/callback', passport.authenticate('google', { failureRedirect: '/users/login' }),callback) 
-router.get('/login/github/callback', passport.authenticate('github', { failureRedirect: '/users/login' }),callback)
-module.exports = router
