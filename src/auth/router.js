967b2bb2e82b6d471c45812b2f1672c944941f5b
Duc Hoang <duchoang206h@gmail.com>
2022-06-06 19:42:28 +0700

    chore: fix register, login, report

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,9 +1,25 @@
 const router = require('express').Router()
-const { callback } = require('./callback')
-const passport = require('./passport')
+const passport = require('./passport');
+const authService = require('./controller')
 router.get('/login/google',passport.authenticate('google', { scope: ['email','profile','openid'] })) 
 router.get('/login/github',passport.authenticate('github', { scope: ['user:email'] }))
 
-router.get('/login/google/callback', passport.authenticate('google', { failureRedirect: '/users/login' }),callback) 
-router.get('/login/github/callback', passport.authenticate('github', { failureRedirect: '/users/login' }),callback)
+router.get('/login/google/callback', passport.authenticate('google', { failureRedirect: '/users/login' }), authService.loginWithSocailCallback) 
+router.get('/login/github/callback', passport.authenticate('github', { failureRedirect: '/users/login' }), authService.loginWithSocailCallback)
+router.get('/login', authService.getLogin);
+router.get('/register', authService.getRegister);
+router.post('/register', authService.register);
+// Users Login Post Route
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        successFlash: true,
+        successRedirect: '/users/dashboard',
+        failureFlash: true,
+        failureRedirect: '/users/login',
+    }),
+
+);
+
+//router.get('/logout', userController.logout);
 module.exports = router
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     ROLE_AMIN :1,
     ROLE_USER :2,
-
+    BCRYPT_SALT_ROUNDS: 10
 }
