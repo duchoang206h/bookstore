c62935bfb1f79bee7e54d4f49128ced173f1f9e6
Duc Hoang Ho <duchoang206h@gmail.com>
2022-06-08 16:07:38 +0700

    fix(database): fix postgres unauthorize error

--- src/models/index.js
+++ src/models/index.js
@@ -8,12 +8,41 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {...config, logging: false});
+
+const sequelize = new Sequelize(config['DB_URL'], 
+                  {
+                    logging: false, 
+                    dialectOptions: {
+                    ssl: {
+                      require: true, 
+                      rejectUnauthorized: false 
+                    }
+                    }
+                  })
+/* if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], 
+    {
+    logging: false, 
+    dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  }
+    });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {...config, logging: false});
-}
+ // sequelize = new Sequelize(config.database, config.username, config.password, {...config, logging: false});
+ sequelize = new Sequelize('postgres://jiftgzsagynlbp:19a6290afb58209ad3c05ab98f420c67d827fbaf09a894fb7fc426bda4258f17@ec2-44-196-174-238.compute-1.amazonaws.com:5432/ddd7g7d4j5jcva', 
+ {
+  logging: false, 
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  }
+});
+} */
 
 fs
   .readdirSync(__dirname)
