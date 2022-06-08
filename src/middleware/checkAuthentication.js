b241b4f2b64ae0b64cbb7c20c014a0370e25ad33
Duc Hoang Ho <duchoang206h@gmail.com>
2022-06-08 14:43:32 +0700

    chore: remove garbage files

--- src/middleware/checkAuthentication.js
+++ src/middleware/checkAuthentication.js
@@ -2,7 +2,6 @@ module.exports = function(req, res, next){
     if(req.isAuthenticated()){
         next();
     }else{
-     //   req.flash('error', `You Need to be logged in First`);
         res.redirect('/auth/login');
     }
 };
