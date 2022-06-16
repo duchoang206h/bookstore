f5f24877021b78bd2336a5e0b14ded9d34a3d364
duchoang206h <duchoang206h@gmail.com>
2022-06-17 02:14:16 +0700

    feat: fix pagination error

--- src/book/controller.js
+++ src/book/controller.js
@@ -1,10 +1,14 @@
 
 const bookService = require("./service");
 const categoryService = require('../category/categoryService');
+const { ITEMS_PER_PAGE } = require('../constants')
 class BookController {
  index = async (req, res) =>{
      const categories = await categoryService.findAll();
-     const pages = 4
+     const countBook = await bookService.count();
+     const pages = (countBook - Math.floor(countBook/ITEMS_PER_PAGE)*ITEMS_PER_PAGE) > 0 ? Math.floor(countBook/ITEMS_PER_PAGE)+1 :Math.floor(countBook/ITEMS_PER_PAGE)
+     const page = req.query.page || 1;
+     const skip = (page -1)* ITEMS_PER_PAGE;
      if(req.query.search){
          const books = await bookService.searchByTitle(req.query.search)
          return  res.render('books/home', { books, categories });
@@ -13,7 +17,7 @@ class BookController {
         const books = await bookService.searchByCategory(req.query.category)
         return  res.render('books/home', { books, categories});
      }
-     const books = await bookService.findAll();
+     const books = await bookService.findAll({ limit: ITEMS_PER_PAGE, offset: skip});
      res.render('books/home', { books, categories, pages});
  }
  bookDetail = async (req, res) => {
